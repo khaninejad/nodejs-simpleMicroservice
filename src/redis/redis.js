c18cca63acb219854e06a1bda686147aa042b642
Payam Khaninejad <khaninejad@gmail.com>
2021-08-04 19:35:55 +0430

    redis refactor for data validation

--- src/redis/redis.js
+++ src/redis/redis.js
@@ -1,47 +1,62 @@
 var connect = require('./connect');
 
+
 var redisDB = {
     client: connect.redis(),
 
-
-
     add_set: function (dset,callback){
-          var key = dset.key.id
-        this.client.hmset(key,dset.data,function(err,data){
-            if(err) return callback(err);
-            else{
-                return callback(true);
-            }
-        });
+        if (!('key' in dset) || !('data' in dset) || !('id' in dset.key) ){
+             return callback(false);
+        }else{
+            var key = dset.key.id
+            this.client.hmset(key,dset.data,function(err,data){
+                if(err) return callback(err);
+                else{
+                    return callback(true);
+                }
+            });
+        }
+        
     },
 
     get_set: function(key, field,callback){
-        this.client.hgetall(key.id, function(err,data){
-            if(err) return false;
-            else {
-                if (field) {
-                    data = data.field;
+        if (!('id' in key) || key.id == ""  ){
+            return callback(false);
+        }else{
+            this.client.hgetall(key.id, function(err,data){
+                if(err) return false;
+                else {
+                    if (field) {
+                        data = data.field;
+                    }
+                    return callback(data);
                 }
-                return callback(data);
-            }
-        });
+            });
+        }
+        
     },
 
     delete_set: function(key,callback){
-        this.client.del(key,function(err,reply){
-            if(err) return false;
-            else if(reply == "null"){
-               return false;
-            }else{
-                callback(reply)
-            }
-                
-        });
+        if (!('id' in key) || key.id == ""  ){
+            return false;
+        }else{
+            this.client.del(key,function(err,reply){
+                if(err) return false;
+                else if(reply == "null"){
+                   return false;
+                }else{
+                    callback(reply)
+                }
+                    
+            });
+        }
+        
     },
-    delete_all: function(key,callback){
+    delete_all: function(){
         this.client.flushdb(function(err){
             if(err) return false;
         });
+       return true
     }
 };
 
